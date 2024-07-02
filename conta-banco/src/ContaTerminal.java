d09f99062dceadc333f80cd852087d6dc8089275
devdouglasm <devdouglasm@gmail.com>
2024-07-02 19:17:33 -0300

    criacao da logica da conta

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        int numeroConta;
+        String agencia = "";
+        String nomeCliente = "";
+        double saldo;
+
+        System.out.println("Digite seu nome por favor: ");
+        nomeCliente = scanner.next();
+
+        System.out.println("Digite sua agência: ");
+        agencia = scanner.next();
+
+        System.out.println("Digite o numero da sua conta: ");
+        numeroConta = scanner.nextInt();
+
+        System.out.println("Digite o seu saldo: ");
+        saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
